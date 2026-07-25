95876ad265ce64d282b5927a1152d3f6fce479c5
pr0uxx <michow289@hotmail.com>
2026-07-25 14:14:32 +0100

    Log if debug mode is enabled

--- Prx.Camera/Services/BaseStationService.cs
+++ Prx.Camera/Services/BaseStationService.cs
@@ -1,11 +1,20 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Prx.Camera.Records;
 
 namespace Prx.Camera.Services;
 
-public class BaseStationService(ITcpListenerService listener) : BackgroundService
+public class BaseStationService(
+    ITcpListenerService listener,
+    ILogger<BaseStationService> logger,
+    IOptions<PrxCameraOptions> options
+    ) : BackgroundService
 {
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        logger.LogInformation("BaseStation service started");
+        logger.LogInformation("Debug = {Debug}", options.Value.Debug);
         return listener.StartAsync(stoppingToken);
     }
 }
